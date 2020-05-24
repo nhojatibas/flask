61712d2c27498a289281e84fe09b7f5ad4add4ec
nhojatibas <nhojatibas@gmail.com>
2020-05-24 16:28:22 -0300

    Append - final da aula de fomularios

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -7,18 +7,27 @@ from app.models.forms import LoginForm
 @app.route("/index")
 @app.route("/")
 def index():
+    print("Index")
     return render_template('index.html')
 
 @app.route("/main")
 def main():
+    print("Nerso")
     return render_template('main.html')
 
-@app.route("/login", methods=["POST", "GET"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    print("Form"+str(form))
+    app.logger.warning('Form..: '+str(form))
     if form.validate_on_submit():
-        print(form.username.data)   
-        print(form.password.data)
+        app.logger.warning('Username..: '+str(form.username.data))
+        app.logger.warning('Senha.....: '+str(form.password.data))
     return render_template('login.html',
                             form=form)
+
+@app.route('/print')
+def printMsg():
+    app.logger.warning('testing warning log')
+    app.logger.error('testing error log')
+    app.logger.info('testing info log')
+    return "Check your console"
